523c77e0c9e7a8c33d165200c1c2e638c22de56d
M A Anson <ansonantony.work@gmail.com>
2025-08-05 12:17:41 +0530

    Update views.py

--- employee_data/views.py
+++ employee_data/views.py
@@ -5,7 +5,7 @@ from .models import Employee
 
 def employee_list(request):
     context = {'employee_list': Employee.objects.all()}
-    return render(request, "employee_data/employee_list.html", context)  # Use forward slashes
+    return render(request, "employee_data/employee_list.html", context)  
 
 def employee_form(request, id=0):
     if request.method == "GET":
@@ -13,14 +13,14 @@ def employee_form(request, id=0):
             form = EmployeeForm()
         else:
             employee = get_object_or_404(Employee, pk=id)
-            form = EmployeeForm(instance=employee)  # FIX: was 'isinstance'
+            form = EmployeeForm(instance=employee)  
         return render(request, "employee_data/employee_form.html", {'form': form})
     else:
         if id == 0:
             form = EmployeeForm(request.POST)
         else:
             employee = get_object_or_404(Employee, pk=id)
-            form = EmployeeForm(request.POST, instance=employee)  # FIX: was 'isinstance'
+            form = EmployeeForm(request.POST, instance=employee)  
         if form.is_valid():
             form.save()
         return redirect("/employee/list")
